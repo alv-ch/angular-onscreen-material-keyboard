b522c34cbe1ab7158201d4774a23e9ccbabb2ba1
seco-plr <casimir.platzer@seco.admin.ch>
2022-07-01 17:14:31 +0200

    Feature/DF-4690 update onscreen keyboard (#3)
    Re-generated project using Angular 13
    
    * Update travis build scripts for ALV
    * Add deployment to @alvch repository

---
+++ projects/angular-onscreen-material-keyboard/src/public-api.ts
@@ -0,0 +1,31 @@
+export * from './lib/classes/keyboard-ref.class';
+
+export * from './lib/components/keyboard/keyboard.component';
+export * from './lib/components/keyboard-container/keyboard-container.component';
+export * from './lib/components/keyboard-key/keyboard-key.component';
+
+export * from './lib/configs/keyboard.config';
+export * from './lib/configs/keyboard-deadkey.config';
+export * from './lib/configs/keyboard-icons.config';
+export * from './lib/configs/keyboard-layouts.config';
+
+export * from './lib/directives/keyboard.directive';
+
+export * from './lib/enums/keyboard-class-key.enum';
+export * from './lib/enums/keyboard-modifier.enum';
+export * from './lib/enums/keyboard-animation-state.enum';
+export * from './lib/enums/keyboard-animation-transition.enum';
+
+export * from './lib/interfaces/keyboard-deadkeys.interface';
+export * from './lib/interfaces/keyboard-icons.interface';
+export * from './lib/interfaces/keyboard-layout.interface';
+export * from './lib/interfaces/keyboard-layouts.interface';
+export * from './lib/interfaces/locale-map.interface';
+
+export * from './lib/pipes/kebab-case.pipe';
+
+export * from './lib/services/keyboard.service';
+
+export * from './lib/utils/keyboard.utils';
+
+export * from './lib/keyboard.module';
